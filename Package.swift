baa7ac8027ffdbf47334d3746f47e01834e60a1d
hytfjwr <f.hayato0104@gmail.com>
2026-03-24 18:01:07 +0900

    chore: bump StatusBarKit to v1.3.0
    Required for ShellCommandResult and runWithResult API.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     name: "StatusBar",
     platforms: [.macOS(.v26)],
     dependencies: [
-        .package(url: "https://github.com/hytfjwr/StatusBarKit", from: "1.2.0"),
+        .package(url: "https://github.com/hytfjwr/StatusBarKit", from: "1.3.0"),
         .package(url: "https://github.com/jpsim/Yams", from: "6.2.1"),
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.0"),
     ],
