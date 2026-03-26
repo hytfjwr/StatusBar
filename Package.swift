0dac4ef1f4944f3c679da10bf4fb8e9f5a6df92a
hytfjwr <f.hayato0104@gmail.com>
2026-03-27 01:32:36 +0900

    chore: bump StatusBarKit dependency to v1.6.0
    Required for the trigger event system: EventRouter, PluginEvent,
    JSONValue, and IPCCommand.trigger are all provided by StatusBarKit 1.6.0.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     name: "StatusBar",
     platforms: [.macOS(.v26)],
     dependencies: [
-        .package(url: "https://github.com/hytfjwr/StatusBarKit", from: "1.5.0"),
+        .package(url: "https://github.com/hytfjwr/StatusBarKit", from: "1.6.0"),
         .package(url: "https://github.com/jpsim/Yams", from: "6.2.1"),
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
